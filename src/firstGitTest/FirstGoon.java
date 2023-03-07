0531d5dacae6462ea366bce7cd6ff08e2a3b57d8
NiYa <NiYa@13931.com>
2023-03-07 18:57:33 +0800

    Second Commit

--- src/firstGitTest/FirstGoon.java
+++ src/firstGitTest/FirstGoon.java
@@ -3,5 +3,6 @@ package firstGitTest;
 public class FirstGoon {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
